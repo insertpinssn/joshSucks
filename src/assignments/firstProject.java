5cf17b1703fa6ba482d477d5f652eaefb44cd289
insertpinssn <iinsertpinssn@gmail.com>
2014-12-23 19:25:00 -0500

    Update firstProject.java

--- src/assignments/firstProject.java
+++ src/assignments/firstProject.java
@@ -1,7 +1,7 @@
 class joshSucks {
     public static void main (String[] args) {
-        System.out.print ("Hello world!");
-        System.out.print ("Josh Sucks! you will be destroyed with the wrath of a thousand suns");
+        System.out.println ("Hello world!");
+        System.out.println ("Josh Sucks! you will be destroyed with the wrath of a thousand suns");
     }
 }
 
